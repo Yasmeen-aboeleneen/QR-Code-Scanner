1d1845f11ba199817130b012f7222c5640eb7280
Yasmeen Abo Eleneen <yasmeenaboeleneen3@gmail.com>
2023-08-11 05:54:20 +0300

    Create HomeScreen Body

--- lib/Home/Home_Screen_body.dart
+++ lib/Home/Home_Screen_body.dart
@@ -5,38 +5,56 @@ class HomeScreenBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child:  Scaffold(
-   body: Container(
-    height: double.infinity,
-    width: double.infinity,
-     child: Stack(
-           children: [
-            Positioned(
-                top: 0,
-                left: 0,
-                child: Image.asset(
-                  'Assets/Images/s_top.png',
-                  height: 150,
-                )),
-            
-            
-             Positioned(
-              bottom: 0,
-              right: 0,
-              child: Image.asset(
-                'Assets/Images/bottom.png',
-                height: 150,
+    return Scaffold(
+      body: Column(children: [
+        const SizedBox(
+          height: 45,
+        ),
+        Center(
+          child: Column(
+            children: [
+              Image.asset(
+                'Assets/Images/picc.jpg',
+                width: 240,
               ),
+            ],
+          ),
+        ),
+        const SizedBox(
+          height: 100,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 88, 125, 117),
+                  side: const BorderSide(color: Colors.orange, width: 1),
+                  shape: const BeveledRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                  textStyle: const TextStyle(
+                      fontSize: 25, fontWeight: FontWeight.bold)),
+              child: const Text('Create QR code'),
             ),
-           ],
-   
-   
-     ),
-   ),
-
-
-
-
-    ));
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 88, 125, 117),
+                  side: const BorderSide(color: Colors.orange, width: 1),
+                  shape: const BeveledRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                  textStyle: const TextStyle(
+                      fontSize: 28, fontWeight: FontWeight.bold)),
+              child: const Text('Scan QR code'),
+            )
+          ],
+        )
+      ]),
+    );
   }
 }
