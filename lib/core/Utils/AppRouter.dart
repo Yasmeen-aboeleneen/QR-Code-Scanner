d089f33c14745886e85acfc11ab07270ad3ffb64
Yasmeen Abo Eleneen <yasmeenaboeleneen3@gmail.com>
2023-09-11 06:55:39 +0300

    edit

--- lib/core/Utils/AppRouter.dart
+++ lib/core/Utils/AppRouter.dart
@@ -1,12 +1,14 @@
 import 'package:go_router/go_router.dart';
 import 'package:qr_scanner/Home/Home_Screen.dart';
 import 'package:qr_scanner/PDF/CreatePdf.dart';
+ 
 
 import '../../Splash/Splash_Screen.dart';
 
 abstract class AppRouter {
   static const KhomeScreen = '/homeScreen';
   static const KcreatePdf = '/createPdf';
+   
 
   // GoRouter configuration
   static final router = GoRouter(
@@ -23,6 +25,7 @@ abstract class AppRouter {
         path: KcreatePdf,
         builder: (context, state) => const CreatePDF(),
       ),
+      
     ],
   );
 }
